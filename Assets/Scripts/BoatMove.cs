8669ea6a67bb85c12dbc20e1fd404ad24070c448
Mary Li <ml4372@nyu.edu>
2019-02-12 16:17:48 -0500

    v1

---
+++ Assets/Scripts/BoatMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BoatMove : MonoBehaviour
+{
+    private SpriteRenderer SR;
+
+    public Text healthDispaly;
+
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision detected");
+        if (other.transform.CompareTag("Hazard"))
+        {
+            SR.color = Color.red;
+            healthDispaly.text = "Health: "+ --health;
+            Debug.Log("Ouch");
+            if (health < 1)
+            {
+                SceneManager.LoadScene("Calibration");
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        SR.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/HazardGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardGenerator : MonoBehaviour
+{
+    public GameObject hazardPrefab1;
+
+    public GameObject[] hazards;
+
+    public int numHazards = 5;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hazards = new GameObject[numHazards];
+        for (int i = 0; i < numHazards; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-5,5), (i+2)*5);
+            hazards[i] = Instantiate(hazardPrefab1, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject Darrel in hazards)
+        {
+            Darrel.transform.position += Vector3.down * 0.1f;
+            if (Darrel.transform.position.y < -10)
+            {
+                Darrel.transform.position = new Vector3(Random.Range(-5,5), Random.Range(5,10));
+            }
+        }
+    }
+}
